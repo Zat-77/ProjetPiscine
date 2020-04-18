44fac26f06a14470197cab1b2f638427efd97b18
Kalmuns <49645027+Kalmuns@users.noreply.github.com>
2020-04-18 18:28:13 +0200

    Menu v1 a changer pour le.php

--- Menu.php
+++ Menu.php
@@ -2,14 +2,6 @@
 
 
 
-
-
-
-
-
-
-
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -23,19 +15,60 @@
 
     <div class="collapse navbar-collapse" id="myNavbar">
     <?php
-    	$user="nonlog";
+    	$user="acheteur";
 		if($user=="nonlog")
-		{
-   		 echo " <ul class="nav navbar-nav">
-      	   <li><a href="Home.html">Home</a></li>
-      	   <li><a href="Produit.html">Products</a></li>
-   		   </ul>
-          <ul class="nav navbar-nav navbar-right">
-           <li><a href="PanierAchatImmediat.html"><span class="glyphicon glyphicon-shopping-cart"></span>Panier</a></li>
-           <li><a href="EditSeller.html">Compte</a></li>
-           <li><a href="Home.html">Logout</a></li>
+		 {
+			 echo " <ul class='nav navbar-nav'>
+             <li><a href='Home.html'>Home</a></li>
+             <li><a href='Produit.html'>Products</a></li>
+              </ul>
+          <ul class='nav navbar-nav navbar-right'>
+           <li><a href= 'Login.html'><span class='glyphicon glyphicon-log-in'></span>Log in</a></li>
           </ul>";
-  		}
+         
+         }
+         if($user=="admin")
+         {
+        	 echo " <ul class='nav navbar-nav'>
+             		<li><a href='Home.html'>Home</a></li>
+            		 <li><a href='Produit.html'>Products</a></li>
+              </ul>
+          	<ul class='nav navbar-nav navbar-right'>
+           		<li><a href='PanierAchatImmediat.html'><span class='glyphicon glyphicon-shopping-cart'></span>Panier</a></li>
+           		<li><a href='EditSeller.html'>Compte</a></li>
+           		<li><a href='Home.html'>Logout</a></li>
+          </ul>";
+
+
+         }
+
+         if($user=="vendeur")
+         {
+         	         		   	 echo " <ul class='nav navbar-nav'>
+             <li><a href='Home.html'>Home</a></li>
+             <li><a href='ItemRegister.html'>Vendre</a></li>
+             <li><a href='MesObjets.html'>MesObjets</a></li>
+              </ul>
+          <ul class='nav navbar-nav navbar-right'>
+           <li><a href='PanierAchatImmediat.html'><span class='glyphicon glyphicon-shopping-cart'></span>Panier</a></li>
+           <li><a href='EditSeller.html'><span class='glyphicon glyphicon-user'></span>Compte</a></li>
+           <li><a href= 'Login.html'><span class='glyphicon glyphicon-log-out'></span>Log Out</a></li>
+           </ul>";
+         }
+
+         if($user=="acheteur")
+         	{
+         		   	 echo " <ul class='nav navbar-nav'>
+             <li><./a href='Home.html'>Home</a></li>
+             <li><./a href='Produit.html'>Products</a></li>
+              </ul>
+          <ul class='nav navbar-nav navbar-right'>
+           <li><a href='PanierAchatImmediat.html'><span class='glyphicon glyphicon-shopping-cart'></span>Panier</a></li>
+           <li><a href='EditClient.html'><span class='glyphicon glyphicon-user'></span>Compte</a></li>
+           <li><a href= 'Login.html'><span class='glyphicon glyphicon-log-out'></span>Log Out</a></li>
+          </ul>";
+         	}
+
       ?>
     </div>
   </div>
