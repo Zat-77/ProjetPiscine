dcbd019578d5a9299ef97140b9c4c5e1b416b7a5
Kalmuns <49645027+Kalmuns@users.noreply.github.com>
2020-04-19 22:51:11 +0200

    menu 4.4

--- Menu.php
+++ Menu.php
@@ -8,27 +8,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="EbayECE.css"/>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/4.4.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
       <a class="navbar-brand" href="#">Logo</a>
     </div>
 
-    <div class="collapse navbar-collapse" id="myNavbar">
+    
     <?php
     	$user=$_SESSION['TypeUser'];
 		if($user=="nonlog")
 		 {
 			 echo " 
-          <ul class='nav navbar-nav navbar-right'>
+          <ul class='nav navbar-dark navbar-right'>
            <li><a href= 'Login.php'><span class='glyphicon glyphicon-log-in'></span>Log in</a></li>
            <li><a href= 'Register.php'><span class='glyphicon glyphicon-log-in'></span>Log in</a></li>
           </ul>";
@@ -36,14 +32,14 @@
          }
          if($user=="admin")
          {
-        	 echo " <ul class='nav navbar-nav'>
+        	 echo " <ul class='nav navbar-dark'>
              	  	<li><a href='Home.php'>Home</a></li>
             		 <li><a href='AdminProduct.php'>Products</a></li>
                              <li><a href='ItemRegister.php'>Vendre</a></li>
              <li><a href='MesObjets.php'>MesObjets</a></li>
                  <li><a href='Negociation.php'>Negociation</a></li>
               </ul>
-          	<ul class='nav navbar-nav navbar-right'>
+          	<ul class='nav navbar-dark navbar-right'>
            		<li><a href='AdminAdmin'>Admin</a></li>
            		<li><a href='EditSeller.php'>Compte</a></li>
            		<li><a href='Login.php'>Logout</a></li>
@@ -54,13 +50,13 @@
 
          if($user=="vendeur")
          {
-         	         		   	 echo " <ul class='nav navbar-nav'>
+         	         		   	 echo " <ul class='nav navbar-dark'>
              <li><a href='Home.php'>Home</a></li>
              <li><a href='ItemRegister.php'>Vendre</a></li>
              <li><a href='MesObjets.php'>MesObjets</a></li>
              <li><a href='Negociation.php'>Negociation</a></li>
               </ul>
-           <ul class='nav navbar-nav navbar-right'>
+           <ul class='nav navbar-dark navbar-right'>
            <li><a href='EditSeller.php'><span class='glyphicon glyphicon-user'></span>Compte</a></li>
            <li><a href= 'Login.php'><span class='glyphicon glyphicon-log-out'></span>Log Out</a></li>
            </ul>";
@@ -72,7 +68,8 @@
              <li><a href='Home.php'>Home</a></li>
              <li><a href='Produit.php'>Products</a></li>
               </ul>
-          <ul class='nav navbar-nav navbar-right'>
+
+          <ul class='nav navbar-nav '>
            <li><a href='PanierAchatImmediat.php'><span class='glyphicon glyphicon-shopping-cart'></span>Panier</a></li>
            <li><a href='EditClient.php'><span class='glyphicon glyphicon-user'></span>Compte</a></li>
            <li><a href= 'Login.php'><span class='glyphicon glyphicon-log-out'></span>Log Out</a></li>
@@ -80,7 +77,7 @@
          	}
 
       ?>
-    </div>
+
   </div>
 </nav>
 </html>
