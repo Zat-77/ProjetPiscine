3f5866ac57bc999146a0af22c475a42c8d32b5f6
Kruskal <vic.delerme@gmail.com>
2020-04-20 20:03:20 +0200

    Ajout Paiement Immediat

--- SupprimerItemAdmin.php
+++ SupprimerItemAdmin.php
@@ -7,10 +7,10 @@ $db_found = mysqli_select_db($db_handle, $database1);
 $sql="DELETE FROM item WHERE item_id = '$id_Supprimer'";
 
 $result = mysqli_query($db_handle, $sql);
-$sql "DELETE FROM negociation WHERE nego_IDItem='$id_Supprimer'"
+$sql="DELETE FROM negociation WHERE nego_IDItem='$id_Supprimer'";
 $result = mysqli_query($db_handle, $sql);
-$sql "DELETE FROM enchere  WHERE enchere _IDItem='$id_Supprimer'"
+$sql="DELETE FROM enchere  WHERE enchere_IDItem='$id_Supprimer'";
 $result = mysqli_query($db_handle, $sql);
-$sql "DELETE FROM immediat  WHERE immediat _IDItem='$id_Supprimer'"
+$sql="DELETE FROM immediat  WHERE immediat_IDItem='$id_Supprimer'";
 $result = mysqli_query($db_handle, $sql);
 ?>
