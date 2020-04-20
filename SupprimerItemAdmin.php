7919b5767a9779a32ec48fdccdfb5bdf73b7b469
Kruskal <vic.delerme@gmail.com>
2020-04-20 12:10:17 +0200

    Update SupprimerItemAdmin.php

--- SupprimerItemAdmin.php
+++ SupprimerItemAdmin.php
@@ -5,9 +5,12 @@ $db_handle = mysqli_connect('localhost', 'root', '');
 $db_found = mysqli_select_db($db_handle, $database1);
 
 $sql="DELETE FROM item WHERE item_id = '$id_Supprimer'";
-echo "id_Supprimer";
-echo "$sql";
+
 $result = mysqli_query($db_handle, $sql);
-
-
+$sql "DELETE FROM negociation WHERE nego_IDItem='$id_Supprimer'"
+$result = mysqli_query($db_handle, $sql);
+$sql "DELETE FROM enchere  WHERE enchere _IDItem='$id_Supprimer'"
+$result = mysqli_query($db_handle, $sql);
+$sql "DELETE FROM immediat  WHERE immediat _IDItem='$id_Supprimer'"
+$result = mysqli_query($db_handle, $sql);
 ?>
